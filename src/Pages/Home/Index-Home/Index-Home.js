1db872f389e69c74f4fbc08d166c5f6adfb02c12
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2023-08-11 20:41:21 +0600

    cmt1

--- src/Pages/Home/Index-Home/Index-Home.js
+++ src/Pages/Home/Index-Home/Index-Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import HomeMain from "../Home-Main/home-main";
-import Info from '../Info/Info'
+import Info from '../Info/Info';
 import "./index-home.css";
 export default function Home() {
   return (
