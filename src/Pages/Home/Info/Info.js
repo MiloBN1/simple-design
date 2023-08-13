6a6fd94f154c683ef09aa683cae643f7c2ee6192
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2023-08-13 15:33:50 +0600

    cvv

--- src/Pages/Home/Info/Info.js
+++ src/Pages/Home/Info/Info.js
@@ -4,45 +4,41 @@ import Img from "../../../Assets/img-mainpage/main-2.svg";
 
 export default function Info() {
   return (
-    <>
-      <div className="container">
-        <div className="wrapper">
-          <div className="infoBlock">
-            <h1>Как это работает?</h1>
-            <div className="infoBlocks">
-              <div className="number">01</div>
-              <div className="text-group">
-                <h4>Выбор состава проекта</h4>
-                <p>
-                  Выберите состав вашего проекта: только планировка или
-                  дизайн-проект
-                </p>
-              </div>
-            </div>
-            <div className="infoBlocks">
-              <div className="number">02</div>
-              <div className="text-group">
-                <h4>Выбор состава проекта</h4>
-                <p>
-                  Выберите состав вашего проекта: только планировка или
-                  дизайн-проект
-                </p>
-              </div>
-            </div>
-            <div className="infoBlocks">
-              <div className="number">03</div>
-              <div className="text-group">
-                <h4>Выбор состава проекта</h4>
-                <p>
-                  Выберите состав вашего проекта: только планировка или
-                  дизайн-проект
-                </p>
-              </div>
-            </div>
+    <div className="wrapper">
+      <div className="infoBlock">
+        <h1>Как это работает?</h1>
+        <div className="infoBlocks">
+          <div className="number">01</div>
+          <div className="text-group">
+            <h4>Выбор состава проекта</h4>
+            <p>
+              Выберите состав вашего проекта: только планировка или
+              дизайн-проект
+            </p>
           </div>
-          <img src={Img} alt="img" className="img" />
+        </div>
+        <div className="infoBlocks">
+          <div className="number">02</div>
+          <div className="text-group">
+            <h4>Выбор состава проекта</h4>
+            <p>
+              Выберите состав вашего проекта: только планировка или
+              дизайн-проект
+            </p>
+          </div>
+        </div>
+        <div className="infoBlocks">
+          <div className="number">03</div>
+          <div className="text-group">
+            <h4>Выбор состава проекта</h4>
+            <p>
+              Выберите состав вашего проекта: только планировка или
+              дизайн-проект
+            </p>
+          </div>
         </div>
       </div>
-    </>
+      <img src={Img} alt="img" className="img" />
+    </div>
   );
 }
